9e7c0874fece45418851d9ce96636f82d0bc2600
stak21 <stakashima21@gmail.com>
2018-09-13 20:34:29 +0000

    small_number_adjustments_similar_to_prev_task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 56; i += 1)
+	{
+		for (j = i + 1; j <= 57; j += 1)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
