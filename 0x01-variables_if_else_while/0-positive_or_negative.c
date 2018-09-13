d57e86c5ee0c2a2d0fc94d5df31911639fbd58da
stak21 <stakashima21@gmail.com>
2018-09-13 18:51:13 +0000

    added_a_newline

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%li is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%li is zero\n", n);
+		printf("%i is zero\n", n);
 	}
 	else
 	{
-		printf("%li is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 
 	return (0);
