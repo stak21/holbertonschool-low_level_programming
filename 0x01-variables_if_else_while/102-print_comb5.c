704b220ece058e41e2ddb4d0a00225fd7f4a18bd
stak21 <stakashima21@gmail.com>
2018-09-13 21:15:45 +0000

    similar_to_all_the_other

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i += 1)
+	{
+		for (j = 48; j <= 56; j += 1)
+		{
+			for (k = 48; k <= 57; k += 1)
+			{
+				for (l = j + 1; l <= 57; l += 1)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != 57 | j != 56 | k != 57 || l != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
