e6c6d1ca4f1b0eb1d1ea74b5b4157dddc5d7d226
stak21 <stakashima21@gmail.com>
2018-09-17 19:03:04 +0000

    use_holberton_header_to_putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* main: Entry Point
+*
+* Description: runs a number of functions in order and returns 0
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/**
+	* _putchar - displays a character from the ascii chart
+	* @c: a character
+	*
+	* Return: a character form the ascii chart
+	*/
+
+	int _putchar(char c);
+
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
+
