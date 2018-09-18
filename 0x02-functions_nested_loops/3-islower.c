16b59750fbf606581e7ea88628ed7c06e41266f2
stak21 <stakashima21@gmail.com>
2018-09-18 23:01:28 +0000

    betty-compliant

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,20 @@
 #include <unistd.h>
 /**
  * _islower - checks for lowercase character
- * @int c
+ * @c: the number to check
+ *
+ * Description: c holds the character to check
  *
  * Return: 1 for lowercase
  * otherwise 0
  */
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-      return 1;
-    }
-  else
-    return 0;
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	return (0);
 }
 
