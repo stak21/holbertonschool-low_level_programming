f68de3dbbb19e11dccd86ec25c7810724feaa2e1
stak21 <stakashima21@gmail.com>
2018-09-19 01:10:21 +0000

    changed_d_to_be_negative

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,16 @@
  * Return: the last digit
  */
 
-int-Wall -pedantic -Werror -Wextra _putchar.c 4-main.c 4-isalpha.c -o 4-isalphaprint_last_digit(int n)
+int print_last_digit(int n)
 {
 	int d;
 
-	if (n < 0)
-	{
-		n *= -1;
-	}
+
 	d = n % 10;
+	if (d < 0)
+	{
+		d *= -1;
+	}
 	_putchar(d + '0');
 	return (d);
 }
