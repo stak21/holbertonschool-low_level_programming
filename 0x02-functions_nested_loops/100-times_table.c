3f6d87577ac47ea495e3e5746d675ecc2db79f11
stak21 <stakashima21@gmail.com>
2018-09-19 20:41:17 +0000

    reworked_and_organized

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,36 +19,36 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j += 1)
 		{
-		 _putchar('0');
+			_putchar('0');
 			for (i = 1; i <= n; i += 1)
 			{
-						d = i * j;
-						_putchar(',');
-						_putchar(' ');
-						if (d >= 100) /* triple digits */
-						{
-							k = d / 10 / 10;
-							a = d % 10;
-							s = d / 10 % 10;	
-							_putchar(k + '0');
-							_putchar(s + '0');
-							_putchar(a + '0');
-						}
-						else if (d >= 10 && d < 100) /* double digit */
-						{
-						  k = d / 10;
-						  a = d % 10;
+				d = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (d >= 100) /* triple digits */
+				{
+					k = d / 10 / 10;
+					a = d % 10;
+					s = d / 10 % 10;	
+					_putchar(k + '0');
+					_putchar(s + '0');
+					_putchar(a + '0');
+				}
+				else if (d >= 10 && d < 100) /* double digit */
+				{
+					k = d / 10;
+					a = d % 10;
 
-						  _putchar(' ');
-						  _putchar(k + '0');
-						  _putchar(a + '0');
-						}
-						else /*single digit */
-						{
-							 _putchar(' ');
-							 _putchar(' ');
-							 _putchar(d % 10 + '0');
-						}
+					_putchar(' ');
+					_putchar(k + '0');
+					_putchar(a + '0');
+				}
+				else /*single digit */
+				{
+					 _putchar(' ');
+					 _putchar(' ');
+					 _putchar(d % 10 + '0');
+				}
 			}
 			_putchar('\n');
 		}
