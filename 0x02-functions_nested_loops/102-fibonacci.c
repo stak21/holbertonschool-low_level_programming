2b4ed2e1bbc640da704a3ae4a1b8510c50d47bc3
stak21 <stakashima21@gmail.com>
2018-09-19 21:53:02 +0000

    fib!

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,6 +8,21 @@
 
 int main(void)
 {
+	int i;
+	unsigned long int j;
+	unsigned long int k;
 
-return (0);
+	j = 2;
+	k = 3;
+	printf("1, 2, 3, ");
+	for (i = 3; i < 50; i += 1)
+	{
+		j += k;
+		printf("%lu", j);
+		if (i != 49)
+			printf(", ");
+		k = j;
+	}
+	printf("\n");
+	return (0);
 }
