33138f4b380ac5aa95b26edada90f7b8cdaf5ff8
stak21 <stakashima21@gmail.com>
2018-09-20 19:24:42 +0000

    adding_header

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+int _isupper(int);
+int _isdigit(int);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
