4952faf0f64ba386d46f6180f7d98a6e537b1880
stak21 <stakashima21@gmail.com>
2018-09-20 21:37:59 +0000

    primeNumbers

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 /**
  * main - Entry point
  *
@@ -8,6 +8,22 @@
 
 int main(void)
 {
+	long long int n;
+	long long int i;
+	long int result;
 
-return (0);
+	n = 612852475143;
+	i = 2;
+	while (n != 1)
+	{
+			if (n % i == 0)
+			{
+				result = i;
+				n /= i;
+			}
+			else
+				i += 1;
+	}
+	printf("%li\n", result);
+	return (result);
 }
