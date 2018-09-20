520a1e75606c4b2756cb513ff9f1fe9a61551000
stak21 <stakashima21@gmail.com>
2018-09-20 19:30:53 +0000

    similar_just_digits

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _isdigit - checks to see if a character is a digit 
+ * @c: holds the chracter to check
+ *
+ * Return: 1 if it is a digit
+ * otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
