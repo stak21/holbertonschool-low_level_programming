6f6f815b5a0e7244caeb1eda714be38f39640b1c
stak21 <stakashima21@gmail.com>
2018-09-20 19:43:11 +0000

    prints_numbers_0-9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i += 1)
+	{
+		_putchar(i + '0');
+	}
+	return (0);
+}
