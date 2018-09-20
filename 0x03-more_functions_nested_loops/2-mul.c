6d0627e572864e6e9ab1386cb910ab471cf8124a
stak21 <stakashima21@gmail.com>
2018-09-20 19:35:04 +0000

    return_result

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: holds an integer
+ * @b: holds another integer
+ *
+ * Return: the result
+ */
+
+int mul(int a; int b)
+{
+	return (a * b);
+}
