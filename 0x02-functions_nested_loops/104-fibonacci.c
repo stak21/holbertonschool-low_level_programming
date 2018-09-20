4e01b6838b8a9a378219cfd5d5117c432fa51a06
stak21 <stakashima21@gmail.com>
2018-09-20 07:22:13 +0000

    samething?

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,6 +8,23 @@
 
 int main(void)
 {
+	int i;
+	unsigned long int j;
+	unsigned long int k;
+	unsigned long int l;
 
+	j = 3;
+	k = 2;
+	printf("1, 2, ");
+	for (i = 3; i < 98; i += 1)
+	{
+		l = j;
+		j += k;
+		printf("%lu", j);
+		if (i != 98)
+			printf(", ");
+		k = l;
+	}
+	printf("\n");
 return (0);
 }
