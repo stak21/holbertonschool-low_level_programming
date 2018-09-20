47e9e37defe42dc6cd1084123e5794d5b857a7fe
stak21 <stakashima21@gmail.com>
2018-09-20 20:08:27 +0000

    repeat_10x_print_single_digits_then_special_case_double_digits

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints digits 0-14 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i += 1)
+	{
+		for (j = 0; j <= 14; j += 1)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
