5b072146b879c0d52c3b89f4299616f2990a1234
stak21 <stakashima21@gmail.com>
2018-09-23 07:27:15 +0000

    cleaning

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,22 +9,40 @@
 int main(void)
 {
 	int i;
-	unsigned long int j;
-	unsigned long int k;
-	unsigned long int l;
-
-	j = 3;
-	k = 2;
+	int last_digit;
+	unsigned long int y1, y2, x1, l, x2;
+	y1 = 2;
+	x1 = 1;
 	printf("1, 2, ");
-	for (i = 3; i < 98; i += 1)
+	for (i = 2; i < 98; i += 1)
 	{
-		l = j;
-		j += k;
-		printf("%lu", j);
-		if (i != 98)
-			printf(", ");
-		k = l;
+		if (i > 90)
+		{
+			if (i == 91)
+			{
+				y1 = y1 / 100;
+				x1 = x1 / 100;
+			}
+	
+
+				y2 = y1 % 100;
+				x2 = 1;
+				printf("\ny1: %lu x1: %lu y2: %lu x2: %lu", y1, x1, y2, x2);
+			if (y2 + x2 >= 10000)
+			{
+				y1 += 1;
+			}
+			else
+				last_digit = y2+ x2;
+		}
+		l = y1;
+		y1 += x1;
+		printf("%lu", y1);
+		if (last_digit > 0 && last_digit < 100)
+			printf("%i", last_digit);
+		printf(" ");
+		x1 = l;
 	}
 	printf("\n");
-return (0);
+	return (0);
 }
