ca8da9b1b4cf67876a46acbdd7141d89af60c390
stak21 <stakashima21@gmail.com>
2018-09-25 21:42:05 +0000

    renamed

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -2,54 +2,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/**
- * _atoi - converts a string to an integer
- * @s: holds the string to convert
- *
- * Return: the integer
- */
-
-/*
- * while ! null
- * look for a number
- * increment i
- * once found look for a string
- *at the same time
- *set j to i
- * keep incrementing i until its not a nnumber
- * after that use a for loop to go form i to j
- * 'i' - j * 1 - 1xx depending on multiple which is j - i
- */
 int _atoi(char *s)
 {
 	int i;
-/*	int j;
-	unsigned int result;
-	int temp;
+	int result;
+	int digit;
 	bool isfirstnumber = false;
-*/
-int result;
+	bool negative = false;
+
 	i = 0;
-//	j = 1;
 	result = 0;
-	printf("%d", i);
+
 	while (s[i] != '\0')
 	{
-	/*
-		if (s[i] >= '0' && s[i] <= 9)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-		_putchar(s[i]);
+			if (!isfirstnumber)
+				if (s[i - 1] == '-')
+					negative = true;
 			isfirstnumber = true;
-			temp = s[i] - '0';
-			result += result * j + temp;
-			j *= 10;
+			digit = s[i] - '0';
+			result = result * 10 + digit;
+			printf("result: %i, digit: %i ", result, digit);
 		}
-		if (s[i] < '0' && s[i] > '9')
-			if (isfirstnumber == true)
+		if (s[i] < '0' || s[i] > '9')
+			if (isfirstnumber)
 				break;
-				*/
-				printf("%i", i);
-				i += 1;
+		i += 1;
 	}
+	printf("\n");
+	if (negative)
+		return (-result);
 	return (result);
 }
