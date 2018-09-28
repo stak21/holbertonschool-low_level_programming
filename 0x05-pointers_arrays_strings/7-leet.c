a5fea2bc7124da1fcd30f41eaaccacadf18ad50d
stak21 <stakashima21@gmail.com>
2018-09-28 04:35:21 +0000

    changed my way of using multideminsional array

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -2,13 +2,34 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * leet - encodes a string into leet
+ * @s: holds the string
  *
- * Return: Always 0 (Success)
+ * Return: the pointer to the string
  */
 
-int main(void)
+char *leet(char *s)
 {
+	int i, k;
+	char array[10][2] = {
+	{'a', 4 + '0'}, {'A', 4 + '0'},
+	{'e', 3 + '0'}, {'E', 3 + '0'},
+	{'t', 7 + '0'}, {'T', 7 + '0'},
+	{'o', '0'}, {'O', '0'},
+	{'l', 1 + '0'}, {'L', 1 + '0'}
+	};
 
-return (0);
+	k = 0;
+	while (s[k] != '\0')
+	{
+		i = 0;
+		while (array[i][0] != '\0')
+		{
+			if (s[k] == array[i][0])
+				s[k] = array[i][1];
+			i += 1;
+		}
+		k += 1;
+	}
+	return (s);
 }
