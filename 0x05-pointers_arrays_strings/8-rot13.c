990b20cc0d923a81fd0cdc29804cf74ad99a491a
stak21 <stakashima21@gmail.com>
2018-09-28 06:14:51 +0000

    ocnditionals

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -2,13 +2,26 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * rot13 - encodes and decodes in rot13
+ * @s: holds the string
+ * Return: returns the pointer to the string
  */
 
-int main(void)
+char *rot13(char *s)
 {
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+	while ((s[i] < 'n' && s[i] >= 'a') || (s[i] >= 'A' && s[i] < 'N'))
+	{
+		s[i] = s[i] + 13;
+		i += 1;
+	}
+	if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+		s[i] = s[i] - 13;
 
-return (0);
+	i += 1;
+}
+return (s);
 }
