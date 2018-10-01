046dcbb7eecad5e12068fbc4526b6d26a4da8bd5
stak21 <stakashima21@gmail.com>
2018-10-01 23:16:54 +0000

    get the first occurance

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+* _strstr - locates a substring
+* @haystack: the string
+* @needle: the substring
+* Return: pointer to the beginning of the located substring or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (haystack[i])
 	{
-	
-		return (0);
+		j = 0;
+		while (needle[j] == haystack[i + j])
+		{
+			j += 1;
+		}
+		if (needle[j] == '\0')
+			return ((haystack + i));
+		i += 1;
 	}
+	return (needle);
+}
