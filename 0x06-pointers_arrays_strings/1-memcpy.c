955ad34b28c88b971b3f45afcc10627be808f6a9
stak21 <stakashima21@gmail.com>
2018-10-01 20:23:07 +0000

    iterate though n times and copy from src to dest

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,19 @@
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+ * _memcpy - copies the byes from an address and yanks it to dest
+ * @dest: the destination to copy to
+ * @src: the source to copy from
+ * @n: the number of characters to copy
+ * Return: the pointer to the destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 	{
-	
-		return (0);
+		unsigned int i;
+		::
+		for (i = 0; i < n; i += 1)
+			dest[i] = src[i];
+
+		return (dest);
 	}
