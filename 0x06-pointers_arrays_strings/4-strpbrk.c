f8a8ca5faece9e425ed3b1f4c7b0eaed346a869b
stak21 <stakashima21@gmail.com>
2018-10-01 22:59:52 +0000

    similar to 2

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+* _strpbrk - searches a string for any of a set of bytes
+* @s: holds the string
+* @accept: holds the set of bytes
+* Return: the pointer to the byte in s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	bool found;
+
+	found = false;
+	i = 0;
+
+	while (!found && s[i])
 	{
-	
-		return (0);
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+				return ((s + i));
+			j += 1;
+		}
+		i += 1;
 	}
+	return (NULL);
+}
