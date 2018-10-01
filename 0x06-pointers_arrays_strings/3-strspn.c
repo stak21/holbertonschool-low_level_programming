53d02913c430f981591b490158eb542b5ebcb914
stak21 <stakashima21@gmail.com>
2018-10-01 21:49:31 +0000

    iterate through a string and iterate through the subset comparing and incrementing if found

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -2,11 +2,31 @@
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+* _strspn - gets the length of a prefix substring
+* @s: holds the string to look into
+* @accept: holds the substring of characters to compare to
+* Return: the number of bytes in the initial segment of s
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	int index;
+
+	count = 0;
+	index = 0;
+	while (*s > 'a' && *s < 'z')
 	{
-	
-		return (0);
+		while (accept[index])
+		{
+			if (accept[index] == *s)
+			{
+				count += 1;
+			}
+			index++;
+		}
+		index = 0;
+		s++;
 	}
+	return (count);
+}
