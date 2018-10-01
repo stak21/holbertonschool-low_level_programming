637321309cfed9c0728ab6237284ec307fe7c776
stak21 <stakashima21@gmail.com>
2018-10-01 20:41:28 +0000

    loop through until you find a subset or null and return it

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,18 @@
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+* _strchr - locates a character in a string
+* @s: holds teh string
+* @c: holds the character to look for
+* Return: returns the index of the occurance
+* otherwise returns null
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s)
 	{
-	
-		return (0);
+		s++;
 	}
+	return (s);
+}
