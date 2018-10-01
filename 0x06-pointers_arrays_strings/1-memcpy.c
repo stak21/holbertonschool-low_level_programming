d23746dd3506db11362ab29970e34bf0df40b629
stak21 <stakashima21@gmail.com>
2018-10-01 20:26:19 +0000

    betty compliant

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,10 @@
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-	{
-		unsigned int i;
-		::
-		for (i = 0; i < n; i += 1)
-			dest[i] = src[i];
+{
+	unsigned int i;
 
-		return (dest);
-	}
+	for (i = 0; i < n; i += 1)
+	dest[i] = src[i];
+	return (dest);
+}
