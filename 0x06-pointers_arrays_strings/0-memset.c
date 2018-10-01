71151c9c217e8d06331e910b15fef5a89a482104
stak21 <stakashima21@gmail.com>
2018-10-01 20:05:15 +0000

    iterate through 10 times and add the value to the address

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,2 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+#include "holberton.h"
 
+/**
+ * _memset - sets n bytes of an area with bytes
+ * @s: holds the pointer
+ * @b: the byte to change to
+ * n: the number of addresses to change
+ * Return: the pointer to the address
+ */
+	
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i += 1)
+		*(s + i) = b;
+	return (s);
+}
