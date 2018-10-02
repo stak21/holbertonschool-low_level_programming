c98e0fe8bb21d0f9843126d028fefae7b24a06c8
stak21 <stakashima21@gmail.com>
2018-10-02 01:42:52 +0000

    set the pointer address to to

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -2,11 +2,12 @@
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
-	{
-	
-		return (0);
-	}
+* set_string - sets the value of a pointer to a char
+* @s: holds the pointer
+* @to: holds the char to
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
