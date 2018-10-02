b9ab6917d5b786129dd9e016f6538990ef366878
stak21 <stakashima21@gmail.com>
2018-10-02 00:33:39 +0000

    prints chessboard

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -2,11 +2,21 @@
 #include "holberton.h"
 
 /**
-	* temp
-	*/
-	
-temp()
+* print_chessboard - prints the chessboard
+* @a: holds the range of characters to print
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i += 1)
 	{
-	
-		return (0);
+		for (j = 0; j < 8; j += 1)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
 	}
+}
