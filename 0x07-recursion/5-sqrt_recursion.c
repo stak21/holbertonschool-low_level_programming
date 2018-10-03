4202b227efff0f9d67a5409ccb040336a087294e
stak21 <stakashima21@gmail.com>
2018-10-03 20:53:12 +0000

    recursion to find the sqrt of a number

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,12 +2,40 @@
 #include "holberton.h"
 
 /**
-*
-*
+* _sqrt_recursion - return the sqrt of a number
+* @n: the number to sqrt
+* Return: the sqrt number
 */
 
-()
+int _sqrt_recursion(int n)
 {
+	int actual_sqrt(int, int);
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt(1, n));
+}
 
-	return (0);
+/**
+* actual_sqrt - a helper function
+* @i: incremental number
+* @cmp: holds the main number
+* Return: sqrt or -1
+*/
+int actual_sqrt(int i, int cmp)
+{
+	int result;
+
+	if (i * i == cmp)
+	{
+		result = i;
+	}
+	else if (i > cmp)
+	{
+		return (-1);
+	}
+	else
+	{
+		result = actual_sqrt(i + 1, cmp);
+	}
+	return (result);
 }
