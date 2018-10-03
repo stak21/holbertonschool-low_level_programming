12f1d15ba4d59dd4ec0116b4d09413f3d5368883
stak21 <stakashima21@gmail.com>
2018-10-03 21:43:45 +0000

    Betty compliant

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -2,19 +2,15 @@
 #include "holberton.h"
 
 /**
-* _strlen_recursion - returns the length of a string using recursion
+* _strlen_recursion - returns the length of a string
 * @s: holds the string
-* Return: returns the length of the string
+* Return: the length of the string
 */
 
 int _strlen_recursion(char *s)
 {
-	int len;
-
 	if (*s != '\0')
-	{
-		len = _strlen_recursion(++s);
-		len++;
-	}
-	return (len);
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
