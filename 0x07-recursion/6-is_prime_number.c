e65cd68910401b24b0ac79395babeb77e5bb04c4
stak21 <stakashima21@gmail.com>
2018-10-03 22:03:59 +0000

    useing a helper function

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -4,10 +4,30 @@
 /**
 * is_prime_number - checks if the number is prime
 * @n: holds the number
+* Return: returns true or false
 */
 
 int is_prime_number(int n)
 {
-	if (
-	return (0);
+	int actual_is_prime(int, int);
+	if (n <= 1)
+		return (0);
+	return (actual_is_prime(n, 2));
+}
+
+/**
+* actual_is_prime - helper function
+* @n: holds the number
+* @mod: the modding number
+* Return: true or false
+*/
+
+int actual_is_prime(int n, int mod)
+{
+	if (mod >= n)
+		return (1);
+	else if (n % mod == 0)
+		return (0);
+	else
+		return (actual_is_prime(n, mod + 1));
 }
