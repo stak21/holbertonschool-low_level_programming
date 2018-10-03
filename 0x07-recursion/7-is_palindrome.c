52d5e35b9d45a52c7eac00be8045431df27aedcc
stak21 <stakashima21@gmail.com>
2018-10-03 23:14:51 +0000

    betty compliant

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -4,6 +4,7 @@
 /**
 * is_palindrome - checks if a string is palindrome
 * @s: holds the string
+* Return: true or false
 */
 
 int is_palindrome(char *s)
@@ -16,6 +17,11 @@ int is_palindrome(char *s)
 	len = get_len(s) - 1;
 	return (actually_is_palin(s, len));
 }
+/**
+* get_len - gets the length of the string
+* @s: holds the string to check
+* Return: the length
+*/
 
 int get_len(char *s)
 {
@@ -24,6 +30,12 @@ int get_len(char *s)
 	else
 		return (0);
 }
+/**
+* actually_is_palin - helper function for actually is palin
+* @s: the pointer to the string
+* @len: holds the length of the string
+* Return: true or false
+*/
 
 int actually_is_palin(char *s, int len)
 {
