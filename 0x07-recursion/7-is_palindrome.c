209470a9b53ed1d10d9a8a80fe3856ed5e11f0ff
stak21 <stakashima21@gmail.com>
2018-10-03 23:08:40 +0000

    check if its a palindrome

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -8,6 +8,34 @@
 
 int is_palindrome(char *s)
 {
-	
-	return (0);
+	int len;
+
+	int get_len(char *s);
+	int actually_is_palin(char *s, int);
+
+	len = get_len(s) - 1;
+	return (actually_is_palin(s, len));
+}
+
+int get_len(char *s)
+{
+	if (*s != '\0')
+		return (1 + get_len(s + 1));
+	else
+		return (0);
+}
+
+int actually_is_palin(char *s, int len)
+{
+	if (len > 0)
+	{
+		if (*s == *(s + len))
+		{
+			len -= 1;
+			actually_is_palin(s + 1, len);
+		}
+		else
+			return (0);
+	}
+	return (1);
 }
