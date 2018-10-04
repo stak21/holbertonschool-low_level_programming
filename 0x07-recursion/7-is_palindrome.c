3d6d6e21a8424ce6f028f9e30c57e6220928f2b4
stak21 <stakashima21@gmail.com>
2018-10-04 06:46:17 +0000

    added index

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -12,10 +12,10 @@ int is_palindrome(char *s)
 	int len;
 
 	int get_len(char *s);
-	int actually_is_palin(char *s, int);
+	int actually_is_palin(char *s, int, int);
 
 	len = get_len(s) - 1;
-	return (actually_is_palin(s, len));
+	return (actually_is_palin(s, len, 0));
 }
 /**
 * get_len - gets the length of the string
@@ -37,14 +37,15 @@ int get_len(char *s)
 * Return: true or false
 */
 
-int actually_is_palin(char *s, int len)
+int actually_is_palin(char *s, int len, int start)
 {
-	if (len >= -1)
+	if (len > start)
 	{
-		if (*s == *(s + len))
+		if (s[start] == s[len])
 		{
 			len -= 1;
-			actually_is_palin(s + 1, len);
+			start += 1;
+			actually_is_palin(s + 1, len, start);
 		}
 		else
 			return (0);
