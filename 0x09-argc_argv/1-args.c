884591e0a95e6a9924744ab6e86b7b15d81f9b50
stak21 <stakashima21@gmail.com>
2018-10-07 22:17:28 +0000

    print argc

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -2,12 +2,15 @@
 #include "holberton.h"
 
 /**
-*
-*
+* main - Entry point
+* @argc: holds the number of arguments passed in
+* @argv: unused
+* Return: Always 0 (Success)
 */
 
-()
+int main(int argc, char **argv)
 {
-
+	(void)argv;
+	printf("%i\n", argc - 1);
 	return (0);
 }
