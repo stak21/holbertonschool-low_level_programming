f6e8c13cd0c45b1ac24629d9d49d5ca42fa6bf60
stak21 <stakashima21@gmail.com>
2018-10-07 22:36:51 +0000

    itertate through the strings using argc as a counter

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -2,12 +2,15 @@
 #include "holberton.h"
 
 /**
-*
-*
+* main - Entry point
+* @argc: unused
+* @argv: contains the strings for passed in arguments
+* Return: Always 0 (Success)
 */
 
-()
+int main(int argc, char **argv)
 {
-
+	while (argc--)
+	printf("%s\n", *argv++);
 	return (0);
 }
