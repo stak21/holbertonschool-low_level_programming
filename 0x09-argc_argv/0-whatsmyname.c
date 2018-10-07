ec09c6bf7e55c52d29834131f4cd2c029ed399ca
stak21 <stakashima21@gmail.com>
2018-10-07 22:04:40 +0000

    using unused to pass by compiler warning and print the name of the file

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#define UNUSED(argc)
 /**
 	* main - Entry point
-	*
+	* @argc: unused
+	* @argv: holds the string of the program name
 	* Return: Always 0 (Success)
 	*/
-	
-int main(int argc, int **argv)
+
+int main(int argc, char **argv)
 	{
 		printf("%s", *argv++);
+		(void)argc;
+		printf("\n");
 		return (0);
 	}
