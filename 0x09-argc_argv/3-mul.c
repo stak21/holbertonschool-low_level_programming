455eb447bdd434128d8e7ac3838ea615947fe0c2
stak21 <stakashima21@gmail.com>
2018-10-08 00:23:45 +0000

    use stdlib to change ascii to integer and multiple

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
-*
-*
+* main - Entry point
+* @argc: unused
+* @argv: holds the strings to the arguments
+* Return: Always 0 (Success)
 */
 
-()
+int main(int argc, char **argv)
 {
-
-	return (0);
+	if (argc >= 3)
+	{
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
