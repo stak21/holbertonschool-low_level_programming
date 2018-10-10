98d4ccb0e01640e5071bf2526aebdf10e7664846
stak21 <stakashima21@gmail.com>
2018-10-10 00:06:03 +0000

    pointer to a pointer!~ of arrays

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
-*
-*
+* alloc_grid - return a pointer to a 2 dimen array
+* @width: holds the integer for width
+* @height: holds the integer for height
+* Return: return a pointer to the array otherwise return NULL
 */
 
-()
+int **alloc_grid(int width, int height)
 {
+	int **ret_ptr;
+	int i;
+	int j;
 
-	return (0);
+	ret_ptr = malloc(sizeof(void *) * height);
+	for (i = 0; i < height; i += 1)
+	{
+		ret_ptr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j += 1)
+			ret_ptr[i][j] = 0;
+	}
+	return (ret_ptr);
 }
