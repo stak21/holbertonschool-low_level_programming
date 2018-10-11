0e81209b5ad83407b3d473d6afbb98aac150eec5
stak21 <stakashima21@gmail.com>
2018-10-11 21:13:32 +0000

    idk

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -3,12 +3,27 @@
 #include "holberton.h"
 
 /**
-*
-*
+* array_range - creates an array of integers
+* @min: holds the minimum amount of values
+* @max: holds the maximum amount of values
+* Return: returns a pointer to the array
 */
 
-()
+int *array_range(int min, int max)
 {
+	int *ret_ptr;
+	int i, size;
 
-	return (0);
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	ret_ptr = malloc(size);
+	if (ret_ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i += 1)
+	{
+		printf("%i\n", size);
+		ret_ptr[i] = min + i;
+	}
+	return (ret_ptr);
 }
