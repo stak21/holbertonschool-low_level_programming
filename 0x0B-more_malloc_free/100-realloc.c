9f582a7125b4aac182ec60e68760c11b1723e387
stak21 <stakashima21@gmail.com>
2018-10-11 23:53:39 +0000

    realloc

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -3,12 +3,40 @@
 #include "holberton.h"
 
 /**
-*
-*
+* _realloc - reallocates a memory area frmo the old size to the new size
+* @ptr: holds the memory address to a previously allocated space
+* @old_size: holds the number of space allocated previously
+* @new_size: holds the number of spaces we need to allocate from the previous
+* Return: a void pointer
 */
 
-()
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *ret_ptr;
+	int i;
+	int len;
 
-	return (0);
+	i = 0;
+	len = new_size - old_size;
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ret_ptr = malloc(sizeof(char) * new_size);
+	if (ret_ptr == NULL)
+		return (NULL);
+	if (!len)
+		len = -len;
+	while (i < len)
+	{
+		*((char *)ret_ptr + i) = *((char *)ptr + i);
+		i += 1;
+	}
+	free(ptr);
+	return (ret_ptr);
 }
