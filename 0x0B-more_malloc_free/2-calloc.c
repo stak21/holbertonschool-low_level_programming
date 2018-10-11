846839ad60253fd5ad172210ada0bac97cda1c45
stak21 <stakashima21@gmail.com>
2018-10-11 19:59:06 +0000

    allocate and set each memory space to 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -3,12 +3,23 @@
 #include "holberton.h"
 
 /**
-*
-*
+* _calloc - allocates memory with nmemb elements of a certain type
+* @nmemb: holds the number of elements in an array
+* @size: holds the size of type of an array
+* Return: null if it fails
 */
 
-()
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *ret_ptr;
+	unsigned int i;
 
-	return (0);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ret_ptr = malloc(size * nmemb);
+	if (ret_ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i += 1)
+		ret_ptr[i] = 0;
+	return (ret_ptr);
 }
