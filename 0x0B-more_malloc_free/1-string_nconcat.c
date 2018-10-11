5c2e7bb33f100960cf442ec00dada0b0ca0bd393
stak21 <stakashima21@gmail.com>
2018-10-11 19:35:47 +0000

    concats 2 strings

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,42 @@
 #include "holberton.h"
 
 /**
-*
-*
+* string_nconcat - concatentates two strings using the newly allocated memory
+* @s1: holds the first string
+* @s2: holds the second string
+* @n: holds the unsigned number of characters to concatenate
+* Return a pointer to the new string
 */
 
-()
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int len, i;
+	unsigned int j;
+	char *ret_ptr;
 
-	return (0);
+	len = 0;
+	i = 0;
+	j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len])
+		len += 1;
+	ret_ptr = malloc(len + n + 1);
+	if (ret_ptr == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		ret_ptr[i] = s1[i];
+		i += 1;
+	}
+
+	while (j < n)
+	{
+		ret_ptr[i + j] = s2[j];
+		j += 1;
+	}
+	return (ret_ptr);
 }
