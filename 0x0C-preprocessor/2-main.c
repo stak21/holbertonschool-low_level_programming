02ba14dd24fa686bb7a7f657fba96cac6b3ba4ab
stak21 <stakashima21@gmail.com>
2018-10-15 22:05:17 +0000

    prints the current file using a macro

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
 
 /**
+* main - Entry point
 *
-*
+* Return: Always 0 (Success)
 */
 
-()
+int main(void)
 {
-
+	printf("%s", __FILE__);
 	return (0);
 }
