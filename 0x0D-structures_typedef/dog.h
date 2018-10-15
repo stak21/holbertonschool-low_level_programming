30c9946707de87200f5874381b581b3424dc0cc9
stak21 <stakashima21@gmail.com>
2018-10-15 23:32:51 +0000

    betty compliant, added a description and included struct dog inside the ifndef

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef dog_header
 #define dog_header
 
-#endif /*dog*/
-
-struct dog {
-	char * name;
+/**
+* struct dog - a struct holding a dogs name, age and the owner of the dog
+* @name: holds the name of the dog
+* @age: holds the age of the dog
+* @owner: holds the pointer to the owner
+*/
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+#endif /*dog*/
