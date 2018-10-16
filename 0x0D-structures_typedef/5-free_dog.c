1544afde97d393154ab0b002f9d6e3d3da009a45
stak21 <stakashima21@gmail.com>
2018-10-16 03:41:56 +0000

    freeing all allocated memory

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "dog.h"
 
 /**
-*
-*
+* free_dog - frees memory of a struct type
+* @d: holds the pointer to the dog
 */
 
-()
+void free_dog(dog_t *d)
 {
-
-	return (0);
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
