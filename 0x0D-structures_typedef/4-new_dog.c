2dacf3d1522c047dc106e81c017244c8577e1049
stak21 <stakashima21@gmail.com>
2018-10-16 03:06:18 +0000

    allocated space for the new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,15 +13,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ret_dog;
+	int i, j, l, k;
 
+	i = j = 0;
 	ret_dog = malloc(sizeof(dog_t));
 	if (ret_dog == NULL)
+	{
 		return (NULL);
-	if (name != NULL)
-		ret_dog->name = name;
-	if (age >= 0)
-		ret_dog->age = age;
-	if (owner != NULL)
-		ret_dog->owner = owner;
+	}
+	while (name[i] != '\0')
+		i += 1;
+	while (owner[j] != '\0')
+		j += 1;
+		printf("%i, %i\n", i, j);
+	ret_dog->name = malloc(i * sizeof(char));
+	ret_dog->owner = malloc(j * sizeof(char));
+	for (k = 0; k < i; k += 1)
+		ret_dog->name[k] = name[k];
+	for (l = 0; l < j; l += 1)
+		ret_dog->owner[0] = owner[l];
+	ret_dog->age = age;
 	return (ret_dog);
 }
