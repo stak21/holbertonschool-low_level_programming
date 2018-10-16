acfa3fe687c445fcdfa2fab5fd377d84c08ba543
stak21 <stakashima21@gmail.com>
2018-10-16 00:30:16 +0000

    prints the structure of a dog and added prototype in dog.h

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "dog.h"
 
 /**
-*
-*
+* print_dog - prints the structure of dog
+* @d: holds the pointer to a dog
 */
 
-()
+void print_dog(struct dog *d)
 {
-
-	return (0);
+	if (d == NULL)
+		return;
+	if(d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %.6f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
