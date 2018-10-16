37ca201ff7be8262b06b139e2d7cba26aa2cc270
stak21 <stakashima21@gmail.com>
2018-10-16 03:26:31 +0000

    freed if malloc returned NULL

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -18,26 +18,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	i = j = 0;
 	ret_dog = malloc(sizeof(dog_t));
 	if (ret_dog == NULL)
-	{
 		return (NULL);
-	}
 	if (name != NULL)
-	{
 		while (name[i] != '\0')
 			i += 1;
-	}
 	if (owner != NULL)
-	{
 		while (owner[j] != '\0')
 			j += 1;
-	}
 	ret_dog->name = malloc(i * sizeof(char) + 1);
+	if (ret_dog->name == NULL)
+	{
+		free(ret_dog);
+		return (NULL);
+	}
 	ret_dog->owner = malloc(j * sizeof(char) + 1);
+	if (ret_dog->owner == NULL)
+	{
+		free(ret_dog->name);
+		free(ret_dog);
+		return (NULL);
+	}
 	for (k = 0; k < i; k += 1)
 		ret_dog->name[k] = name[k];
 	for (l = 0; l < j; l += 1)
-		ret_dog->owner[j] = owner[l];
-	if (age >= 0)
-		ret_dog->age = age;
+		ret_dog->owner[l] = owner[l];
+	ret_dog->age = age;
 	return (ret_dog);
 }
