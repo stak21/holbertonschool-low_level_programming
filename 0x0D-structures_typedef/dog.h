471ff013c46cd5a3d36f78bc01db2d37f96d5e6f
stak21 <stakashima21@gmail.com>
2018-10-16 00:55:14 +0000

    moved where I created typedef struct

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,13 +7,13 @@
 * @age: holds the age of the dog
 * @owner: holds the pointer to the owner
 */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /*dog*/
