a267fd0a7ced52daf3be851931832bc5f7ad0bff
stak21 <stakashima21@gmail.com>
2018-10-17 22:23:07 +0000

    added prototype cmp; cmpared every value in the array to the cmp and returned the index

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "function_pointers.h"
 
 /**
-*
-*
+* int_index - seraches for an integer in an array
+* @array: the array holding the integer
+* @size: holds the size of the array
+* @cmp: the function pointer to a compare
+* Return: 0: found, 1: not -1 not found
 */
 
-()
+int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
-	return (0);
+	i = 0;
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i += 1;
+	}
+	return (-1);
 }
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -2,9 +2,12 @@
 #define header
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int it_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_elem(int elem);
 void print_elem_hex(int elem);
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 #endif
