c9d0cc01e51d2117675c692ece54dd7b9ea00620
stak21 <stakashima21@gmail.com>
2018-10-20 05:05:47 +0000

    iterates through n amounts of arguments

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
-*
-*
+* sum_them_all - returns the sum of all its parameters
+* @n: the number of arguments
+* Return: the sum or if n == 0
 */
 
-()
+int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int result;
+	va_list args;
 
-	return (0);
+	if (n == 0)
+		return (0);
+	result = 0;
+	va_start(args, n);
+	for (i = 0; i < n; i += 1)
+	{
+		result += va_arg(args, int);
+	}
+	va_end(args);
+	return (result);
 }
