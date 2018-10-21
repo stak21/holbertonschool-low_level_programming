66db8f1535567a2f5580d8724f6d3c8dcb677031
stak21 <stakashima21@gmail.com>
2018-10-21 17:47:08 +0000

    moved separator validation to line 27

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -17,14 +17,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *word;
 
 	va_start(arg, n);
-	for (i = 0; i < n && (separator); i += 1)
+	for (i = 0; i < n; i += 1)
 	{
 		word = va_arg(arg, char *);
 		if (word == NULL)
 			printf("(nil)");
 		else if (sizeof(word) == sizeof(void *))
 			printf("%s", word);
-		if (i != n - 1)
+		if (i != n - 1 && separator)
 			printf("%s", separator);
 	}
 	va_end(arg);
