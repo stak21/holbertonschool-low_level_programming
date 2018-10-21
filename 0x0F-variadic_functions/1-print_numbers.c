794ef323ab55cbe89d7e89c35967d9cfabeb34d5
stak21 <stakashima21@gmail.com>
2018-10-21 17:46:39 +0000

    moved separator validation to line 24

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -17,11 +17,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	for (i = 0; i < n && (separator); i += 1)
+	for (i = 0; i < n; i += 1)
 	{
 		if (sizeof(va_arg(arg, int)) == sizeof(int))
 			printf("%i", va_arg(arg, int));
-		if (i != n - 1)
+		if (i != n - 1 && separator)
 			printf("%s", separator);
 	}
 	va_end(arg);
