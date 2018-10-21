1da7fa185918d9d05832c9748478cb6502b640fb
stak21 <stakashima21@gmail.com>
2018-10-21 17:17:06 +0000

    first try

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "variadic_functions.h"
 
 /**
-*
-*
+* print_strings - prints a string
+* @separator: holds a string
+* @n: holds the number of arguments
 */
 
-()
+void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	
+	va_list arg;
+	char *word;
 
-	return (0);
+	if (separator == NULL)
+		return;
+	va_start(arg, n);
+	for (i = 0; i < n; i += 1)
+	{
+		word = va_arg(arg, char *);
+		if (word == NULL)
+			printf("(nil)");
+		else if (sizeof(word) == sizeof(void *))
+			printf("%s", word);
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
 }
