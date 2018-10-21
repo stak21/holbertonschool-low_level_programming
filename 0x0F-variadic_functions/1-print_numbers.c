ad0937fa18d8dcbe891c85d169331b05f8afeabb
stak21 <stakashima21@gmail.com>
2018-10-21 17:16:58 +0000

    Prints numbers

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
-*
-*
+*  print_numbers - only prints numbers 
+* @separator: the string to be printed between numbers
+* @n: holds the number of arguments
 */
 
-()
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 
-	return (0);
+	unsigned int i;
+	va_list arg;
+	
+	if (separator == NULL)
+		return;
+	va_start(arg, n);
+
+	for (i = 0; i < n; i += 1)
+	{
+		if (sizeof(va_arg(arg, int)) == sizeof(int))
+			printf("%i", va_arg(arg, int));
+		if ((i + 1) != n)
+			printf("%s", separator);
+	}
+	printf("\n");
 }
