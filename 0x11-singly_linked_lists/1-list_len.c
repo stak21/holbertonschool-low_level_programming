962eefd902362e8068a13ac01322030d15746550
stak21 <stakashima21@gmail.com>
2018-10-25 23:36:48 +0000

    increment each iteration

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
 
 /**
-*
-*
+* list_len - returns the number of elements in a linked list
+* @h: pointer to the struct linked list
+* Return: the number of elements
 */
 
-()
+size_t list_len(const list_t *h)
 {
+	size_t elements;
 
-	return (0);
+	while (h)
+	{
+		h = h->next;
+		elements += 1;
+	}
+
+	return (elements);
 }
