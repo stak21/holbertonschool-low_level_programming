cdc989d3d6d2bd7fc4bda73147b210415677720b
stak21 <stakashima21@gmail.com>
2018-10-26 01:40:03 +0000

    removed strlen function and manually retrieved length

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -14,14 +14,18 @@ list_t *add_node(list_t **head, const char *str)
 {
 	char *copy_str;
 	list_t *ret_list;
+	unsigned int i;
 
 	ret_list = malloc(sizeof(list_t));
 	if (ret_list == NULL)
 		return (NULL);
 	copy_str = strdup(str);
+	for (i = 0; str[i] != '\0';)
+		i += 1;
+
 	ret_list->str = copy_str;
 	ret_list->next = *head;
-	ret_list->len = strlen(copy_str);
+	ret_list->len = i; 
 	*head = ret_list;
 	return (*head);
 }
