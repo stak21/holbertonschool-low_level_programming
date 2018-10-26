cd353f7e6a82b5601f1886a42a57c9ca06ac4a44
stak21 <stakashima21@gmail.com>
2018-10-26 04:53:04 +0000

    frees linked list

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
 
 /**
-*
-*
+* free_list - frees a linked list
+* @head: a pointer to the head of a linked list
 */
 
-()
+void free_list(list_t *head)
 {
+	list_t *next_ptr;
 
-	return (0);
+	while (head->next != NULL)
+	{
+		next_ptr = head;
+		head = head->next;
+			free(next_ptr);
+	}
+
 }
