53547cf2dccef7306e466075dd05f8f6731b37b9
stak21 <stakashima21@gmail.com>
2018-11-02 04:46:02 +0000

    get the difference and subtract until 0

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -3,12 +3,20 @@
 #include "holberton.h"
 
 /**
-*
-*
+* flip_bits - returns the number of bits you would need to flip to get fmor one
+* number to another
+* @n: the number
+* @m: the 2nd number
+* Return: the count
 */
 
-()
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int count;
+	unsigned int difference;
 
-	return (0);
+	difference = n ^ m;
+	for (count = 0; difference != 0; count += 1)
+		difference &= (difference - 1);
+	return (count);
 }
