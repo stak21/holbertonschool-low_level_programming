706804c1d63094f73466f53f1e8625d849eb82c5
stak21 <stakashima21@gmail.com>
2018-11-02 21:55:42 +0000

    prints before main exe

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
 
 /**
-*
-*
+* printFirst - prints before main is exectuted
 */
 
-()
+void printFirst(void)
 {
-
-	return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
