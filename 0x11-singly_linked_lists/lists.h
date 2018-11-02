f55b69a308e8ed773f59b2b88e9601c81eae431e
stak21 <stakashima21@gmail.com>
2018-11-02 22:04:15 +0000

    changed void

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void printFirst(void) __attribute__((constructor));
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
