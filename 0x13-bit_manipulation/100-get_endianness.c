aa4ea5f80dd2afce1520ea810228093122c9e25e
stak21 <stakashima21@gmail.com>
2018-11-02 07:03:32 +0000

    check if a computer uses little endian or big

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -3,12 +3,18 @@
 #include "holberton.h"
 
 /**
-*
-*
+* get_endianness - checks the endianness of a computer
+* Return: 1 if  little endian
 */
 
-()
+int get_endianness(void)
 {
+	int i;
 
-	return (0);
+	i = 2;
+
+	if (i & 1)
+		return (0);
+	else
+		return (1);
 }
