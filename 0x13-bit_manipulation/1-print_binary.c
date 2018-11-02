76e5717a416f40822d455f4096538412a18384be
stak21 <stakashima21@gmail.com>
2018-11-02 06:49:53 +0000

    recursively print

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -3,12 +3,19 @@
 #include "holberton.h"
 
 /**
-*
-*
+* print_binary - prints a binary representation of a number
+* @n: holds the number to change
 */
 
-()
+void print_binary(unsigned long int n)
 {
+	if ((n >> 1) == 0 && n == 0)
+	{
+		printf("0");
+		return;
+	}
+	if (n >> 1 != 0)
+		print_binary(n >> 1);
+	printf("%lu", n & 1);
 
-	return (0);
 }
