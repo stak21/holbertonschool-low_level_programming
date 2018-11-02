36d27a5a9841ffab6c9743ae4c1370937531426b
stak21 <stakashima21@gmail.com>
2018-11-02 04:05:04 +0000

    get the value of a bit at an index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -3,12 +3,16 @@
 #include "holberton.h"
 
 /**
-*
-*
+* get_bit - gets the value of a bit at a given index
+* @n: holds the number to check
+* @index: the index to check
+* Return: returns the value of the bit
+* otherwise -1
 */
 
-()
+int get_bit(unsigned long int n, unsigned int index)
 {
-
-	return (0);
+	if (index >= 64)
+		return (-1)
+	return ((n & (1 << index)) ? 1: 0);
 }
