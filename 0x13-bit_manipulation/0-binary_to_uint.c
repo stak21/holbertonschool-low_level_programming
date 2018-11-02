2c2aac7c98e723316f7ed048d128a21bed0187bc
stak21 <stakashima21@gmail.com>
2018-11-02 06:10:31 +0000

    shift numbers each digit

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, integer;
-	
+
+	integer = 0;
 	i = 0;
 	if (!b)
 		return (0);
-		
+
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-	
-		
-		
+		if (b[i] & 1)
+			integer += 1;
+		integer <<= 1;
+		i += 1;
 	}
-	return (0);
+	return (integer);
 }
