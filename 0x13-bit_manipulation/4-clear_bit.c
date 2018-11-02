161aa7c0fbb475e5ef4de2db769209daf3f1d56e
stak21 <stakashima21@gmail.com>
2018-11-02 04:25:43 +0000

    inverted the number and AND the results

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -3,12 +3,16 @@
 #include "holberton.h"
 
 /**
-*
-*
+* clear_bit - clears the bit at a given index
+* @n: a pointer to the number
+* @index: the index the change
+* Return success or -1
 */
 
-()
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-
-	return (0);
+	if (index >= 64)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
