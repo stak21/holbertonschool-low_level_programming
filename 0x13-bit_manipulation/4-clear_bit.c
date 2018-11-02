afb9136a9f9cf46026da6f5c295248fb0b1891b4
stak21 <stakashima21@gmail.com>
2018-11-02 04:27:08 +0000

    betty compliant

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
 * clear_bit - clears the bit at a given index
 * @n: a pointer to the number
 * @index: the index the change
-* Return success or -1
+* Return: success or -1
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
