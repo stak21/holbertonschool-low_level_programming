0d342b43724d2f10f6c71bb10de40e2102dfe82a
stak21 <stakashima21@gmail.com>
2018-11-04 23:55:17 +0000

    first attempt

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <stdbool.h>
+#include "lists.h"
 
 /**
-*
-*
+* print_listint_safe - prints a linked list
+* @head: points to the beginning of a linked list
+* Return: the number of nodes in a list
 */
 
-()
+size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *fast_ptr, *slow_ptr;
+	size_t size;
 
-	return (0);
+	size = 0;
+
+	if (head == NULL)
+		return (0);
+
+	slow_ptr = head;
+	fast_ptr = head->next;
+
+	while (fast_ptr != NULL && fast_ptr < slow_ptr)
+	{
+		size += 1;
+	printf("[%p] %i\n", (void *)slow_ptr, slow_ptr->n);
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+	}
+	printf("[%p] %i\n", (void *)slow_ptr, slow_ptr->n);
+
+	return (size);
 }
--- 0x12-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
 
 /**
-*
-*
+* free_listint_safe - frees a linked list safely
+* @h: holds a pointer to a pointer of a linked list
+* Return: the size of the list
 */
 
-()
+size_t free_listint_safe(listint_t **h)
 {
+	listint_t *slow_ptr, *fast_ptr, *free_ptr;
+	size_t size;
 
-	return (0);
+	size = 0;
+
+	if (!h || *h == NULL)
+		return (0);
+	fast_ptr = (*h)->next;
+	slow_ptr = *h;
+
+	while (fast_ptr && fast_ptr < slow_ptr)
+	{
+		free_ptr = slow_ptr;
+		fast_ptr = fast_ptr->next;
+		slow_ptr = slow_ptr->next;
+		size += 1;
+
+		free(free_ptr);
+	}
+	free(slow_ptr);
+	*h = NULL;
+	return (size);
 }
