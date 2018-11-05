7f28f3329f7566eae4f4fde696f971d442bbfe27
stak21 <stakashima21@gmail.com>
2018-11-05 02:53:41 +0000

    forgot the newline

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, str, 59);
 	return (1);
