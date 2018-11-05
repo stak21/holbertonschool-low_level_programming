915e7a5fa54d3aba00519262430a82326ad6ffec
stak21 <stakashima21@gmail.com>
2018-11-05 01:57:13 +0000

    return NULL

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "lists.h"
 
 /**
@@ -12,17 +13,34 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *fast_ptr, *slow_ptr;
+	bool is_loop;
 
 	if (head == NULL)
 		return (NULL);
-	fast_ptr = head->next;
+	fast_ptr = head;
 	slow_ptr = head;
+	is_loop = false;
 
-	while (fast_ptr && fast_ptr < slow_ptr)
+	while (fast_ptr && slow_ptr && fast_ptr->next)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+
+		if (slow_ptr == fast_ptr)
+		{
+			is_loop = true;
+			break;
+		}
+	}
+	if (is_loop)
+	{
+		fast_ptr = head->next;
+		slow_ptr = head;
+	}
+	while (fast_ptr && fast_ptr < slow_ptr && is_loop)
 	{
 		slow_ptr = slow_ptr->next;
 		fast_ptr = fast_ptr->next;
 	}
-
 	return (fast_ptr);
 }
