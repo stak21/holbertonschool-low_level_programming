527918c1fa4c8e214380b7ad77943a2335f7dcba
stak21 <stakashima21@gmail.com>
2018-11-05 02:47:34 +0000

    writes to stdout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, str, strlen(str));
+	return (1);
+}
