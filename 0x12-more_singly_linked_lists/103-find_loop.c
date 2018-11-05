5c031abf685780888c823c779d2796a04cdfb56b
stak21 <stakashima21@gmail.com>
2018-11-05 00:14:47 +0000

    finds the address of the loop

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
 
 /**
-*
-*
+* find_listint_loop - finds the loop in a linked list
+* @head: pointer to a linked list
+* Return: the address of where the loop starts
+* otherwise NULL
 */
 
-()
+listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *fast_ptr, *slow_ptr;
 
-	return (0);
+	if (head == NULL)
+		return (NULL);
+	fast_ptr = head->next;
+	slow_ptr = head;
+
+	while (fast_ptr && fast_ptr < slow_ptr)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+	}
+
+	return (fast_ptr);
 }
