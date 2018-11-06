0b1deccc20ae184d339e7bda690a78f198db078d
stak21 <stakashima21@gmail.com>
2018-11-06 23:44:58 +0000

    Opened a file and read from it. Attemp: 1

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,14 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
 
 /**
-*
-*
+* read_textfile - reads a text file and prints it to stdout
+* @filename: the file name to read from
+* @letters: the number of letters it should read and print
+* Return: the actual number of letters it could read and print
+* otherwise an error return 0
 */
 
-()
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, len, i;
+	char *buf;
 
-	return (0);
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	read(fd, buf, letters);
+	buf[letters] = '\0';
+	write(2, buf, letters);
+	close(fd);
+
+	for (i = 0; buf[i] != '\0'; i += 1)
+	{
+		len += 1;
+	}
+	free(buf);
+
+	return (len);
 }
