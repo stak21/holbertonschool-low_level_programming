1336a5b938afb4f9d2c545ace30cf074c07cdb30
stak21 <stakashima21@gmail.com>
2018-11-06 22:49:32 +0000

    Adding required files

---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
