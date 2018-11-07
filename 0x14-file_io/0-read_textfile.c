471035c4a59f881677d13cff3085c9ebeb984d0d
stak21 <stakashima21@gmail.com>
2018-11-07 02:50:23 +0000

    freed everything

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, len, i;
+	int fd, len;
 	int res;
 	char *buf;
 
@@ -19,29 +19,37 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/* open */
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) *letters);
 	if (!buf)
 		return (0);
 
 	/* read into buf */
 	res = read(fd, buf, letters);
 	if (!res)
+	{
+		free(buf);
 		return (0);
-	buf[letters] = '\0';
-	for (i = 0; buf[i] != '\0'; i += 1)
-	len += 1;
+	}
 
+	buf[letters] = '\0';
+	for (len = 0; buf[len] != '\0';)
+		len += 1;
 	/* close */
 	res = close(fd);
 	if (res != 0)
+	{
+		free(buf);
 		exit(-1);
+	}
 	res = write(STDOUT_FILENO, buf, len);
 	if (res != len)
+	{
+		free(buf);
 		return (0);
+	}
 	free(buf);
 
 	return (len);
