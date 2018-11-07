8f88fa6027998193fa5e3d7b2e74cd14a669c11d
stak21 <stakashima21@gmail.com>
2018-11-07 03:08:04 +0000

    reverted

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -10,46 +10,37 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, len;
+	int fd, len, i;
 	int res;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
-
 	/* open */
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) *letters);
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 
 	/* read into buf */
-	res = read(fd, buf, letters);
-	if (!res)
-	{
-		free(buf);
-		return (0);
-	}
-
+	read(fd, buf, letters);
 	buf[letters] = '\0';
-	for (len = 0; buf[len] != '\0';)
+
+	for (i = 0; buf[i] != '\0'; i += 1)
 		len += 1;
+
 	/* close */
 	res = close(fd);
 	if (res != 0)
-	{
-		free(buf);
 		exit(-1);
-	}
 	res = write(STDOUT_FILENO, buf, len);
 	if (res != len)
-	{
-		free(buf);
 		return (0);
-	}
 	free(buf);
 
 	return (len);
