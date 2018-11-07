bf45c5d9709687a065793b6d7fc0f7b7d2c72e47
stak21 <stakashima21@gmail.com>
2018-11-07 04:51:54 +0000

    intial commit

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,14 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
+#define BUF_SIZE 10
 
 /**
+* main - Entry point
 *
-*
+* Return: Always 0 (Success)
 */
 
-()
+int main(int argc,char **argv)
 {
+	int fd, res, i;
+	char *buf;
 
+	i = 0;
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from filen_to\n");
+		exit(97);
+	}
+	/* open from */
+	fd = open(argv[1], O_RDONLY);	
+	/* make into a func */
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	buf = malloc(sizeof(char) * BUF_SIZE);
+	if (!buf)
+		return (0);
+		
+	/* reads into buffer BUF_SIZE at a time until the whole file is read */
+	do
+	{
+		res = read(fd, buf + i, BUF_SIZE);
+		i += res;
+		if (res == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+	} while (res == BUF_SIZE);
+	
+	res = close(fd);
+	if (res == -1)
+	{
+		dprintf(2, "Error Can't close fd %i\n", fd);
+		exit(100);
+	}
+
+	/* open to */
+	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	res = write(fd, buf, i);
+	if (res != i)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (res == -1)
+	{
+		dprintf(2, "Error Can't close fd %i\n", fd);
+		exit(100);
+	}
+	res = close(fd);
+	if (res == -1)
+	{
+		dprintf(2, "Error Can't close fd %i\n", fd);
+		exit(100);
+	}
 	return (0);
 }
