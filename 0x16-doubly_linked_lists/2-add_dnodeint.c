b970bdc2262363b271337dc9e9ab54a6ca2a788c
stak21 <stakashima21@gmail.com>
2018-12-09 23:34:07 +0000

    inital commit

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
- /**
+/**
 * add_dnodeint - adds a new node at the beginning of a list
 * @head: pointer to the linked list
 * @n: the data in the node
@@ -22,5 +22,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->next = *head;
 	*head = newnode;
 	return (newnode);
-	
+
 }
