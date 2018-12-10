1910ac77ec182892ce079cf5ebbc19ec9a95bd8c
stak21 <stakashima21@gmail.com>
2018-12-10 00:14:27 +0000

    inital commit

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* sum_dlistint - return the sum of all the data
+* @head: the pointer to a linked list
+* Return: the sum
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	dlistint_t *cur_ptr;
+
+	if (head == NULL)
+		return (0);
+	
+	cur_ptr = head;
+	sum = 0;
+
+	while (cur_ptr != NULL)
+	{
+		sum += cur_ptr->n;
+		cur_ptr = cur_ptr->next;
+	}
+
+	return (sum);
+}
