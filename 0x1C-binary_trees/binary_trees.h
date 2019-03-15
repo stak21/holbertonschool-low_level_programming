42aef26e43196cf78263d66411df200ab517abba
stak21 <stakashima21@gmail.com>
2019-03-15 00:41:39 +0000

    Added prototype: binary_trees_ancestor

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -42,6 +41,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, \
+	const binary_tree_t *second);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
