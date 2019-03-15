294515772e35f3a52080d0e1262fd49f59a978df
stak21 <stakashima21@gmail.com>
2019-03-15 00:49:44 +0000

    Added new typedef

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -21,6 +21,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bt;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
