040b712312d879d8321d82cbe81dea28d9ea0ca4
stak21 <stakashima21@gmail.com>
2019-04-17 23:27:54 +0000

    Added headers

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef SEARCH_ALGO_HEADER
 #define SEARCH_ALGO_HEADER
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
